630b40edfc7b98794cd068bbb3ec3946f6031f18
Felipeacs05 <felipedocarmo05@gmail.com>
2026-03-20 16:25:41 +0000

    Implementando colisão em blocos gerados automaticamente

--- Assets/Scripts/MoverObstaculo.cs
+++ Assets/Scripts/MoverObstaculo.cs
@@ -3,13 +3,22 @@ using UnityEngine;
 public class Obstaculo : MonoBehaviour
 {
     public float velocidade = 30f;
+    private Rigidbody rb;
 
-    void Update()
+    void Start()
     {
-        // Move o obstáculo em direção ao jogador (Z negativo)
-        transform.Translate(Vector3.back * velocidade * Time.deltaTime);
+        // Pega o componente Rigidbody que você adicionou no Unity
+        rb = GetComponent<Rigidbody>();
+    }
 
-        // Se ele passou do jogador (Z < -10), some para sempre
+    // Usamos FixedUpdate para cálculos de física (exigência do enunciado!)
+    void FixedUpdate()
+    {
+        // Move o obstáculo fisicamente dando velocidade ao corpo dele
+        // Vector3.back é o mesmo que (0, 0, -1)
+        rb.linearVelocity = Vector3.back * velocidade;
+
+        // Se ele passou do jogador, remove o objeto para economizar memória
         if (transform.position.z < -45f)
         {
             Destroy(gameObject);
